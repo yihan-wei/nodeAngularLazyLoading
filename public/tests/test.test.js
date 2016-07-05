7a5f7b0a2351ebd0c11c113f5f7c74c1299c8655
yihan-wei <wei.yihan89@gmail.com>
2016-07-05 10:29:40 -0400

    tests init commit

---
+++ public/tests/test.test.js
@@ -0,0 +1,12 @@
+describe('test testing framework',function(){
+	beforeEach(module('app'));
+	var $controller;
+	beforeEach(inject(function(_$controller_){
+		$controller =  _$controller_;
+	}));
+
+	descrit('test HomeViewController',function(){
+		it('test 1',function(){
+		})
+	})
+})
